4281f5bb86c7ab78f999e97b3721bb8bf80efac0
BuddhiAbeyratne <buddhi741@gmail.com>
2018-11-10 04:13:37 +0530

    Created Card, Game, and Player Models implementation for server

--- src/io/codenames/serverdata/Card.java
+++ src/io/codenames/serverdata/Card.java
@@ -2,4 +2,81 @@ package io.codenames.serverdata;
 
 public class Card {
 
+    private int type;
+    private String codeName;
+    private boolean hidden = true;
+
+
+    /**
+     * Create Card
+     * @param type
+     * @param codeName
+     * @return Card
+     */
+    protected Card(int type, String codeName) {
+        this.type = type;
+        this.codeName = codeName;
+    }
+
+    /**
+     * Get type of card
+     *
+     * @return
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * Set type of cards
+     *
+     * @param type
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    /**
+     * get codeName of Card
+     *
+     * @return
+     */
+    public String getCodeName() {
+        return codeName;
+    }
+
+    /**
+     * Set codeName of card
+     *
+     * @param codeName
+     */
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+
+    /**
+     * check if card is hidden
+     *
+     * @return
+     */
+    public boolean isHidden() {
+        return hidden;
+    }
+
+    /**
+     * Set card hidden state
+     *
+     * @param hidden
+     */
+    private void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    /*
+     * reveal card andSet hidden state to false
+     */
+    public void revealCard() {
+        setHidden(false);
+    }
+
 }
--- src/io/codenames/serverdata/Player.java
+++ src/io/codenames/serverdata/Player.java
@@ -1,5 +1,98 @@
 package io.codenames.serverdata;
 
 public class Player {
+    private String name;
+    private int numGames;
+    private int cardsReviled;
+    private int correctReviles;
+    private int incorrectReviles;
+
+    /**
+     * Get player name
+     * @return String
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set player name
+     * @param name Player Name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get Number of names played
+     * @return int
+     */
+    public int getNumGames() {
+        return numGames;
+    }
+
+    /**
+     * set number of games
+     * @param numGames Number of Games Played
+     */
+    protected void setNumGames(int numGames) {
+        this.numGames = numGames;
+    }
+
+    /**
+     * Increment number of games played
+     */
+    public void playedGame() {
+        this.numGames++;
+    }
+
+    /**
+     * Get number of cards Reviled
+     * @return int
+     */
+    public int getCardsReviled() {
+        return cardsReviled;
+    }
+
+    /**
+     * Set number of cards reviled by a player
+     * @param cardsReviled Number of cards reviled
+     */
+    protected void setCardsReviled(int cardsReviled) {
+        this.cardsReviled = cardsReviled;
+    }
+
+    /**
+     * Get correct reveals by player
+     * @return int
+     */
+    public int getCorrectReviles() {
+        return correctReviles;
+    }
+
+    /**
+     * Set correct cards reviled
+     * @param correctReviles Number of Correct reviles
+     */
+    protected void setCorrectReviles(int correctReviles) {
+        this.correctReviles = correctReviles;
+    }
+
+    /**
+     * Get incorrect card reviles
+     * @return int
+     */
+    public int getIncorrectReviles() {
+        return incorrectReviles;
+    }
+
+    /**
+     * Set Incorrect Revels
+     * @param incorrectReviles number of incorrect reviles
+     */
+    protected void setIncorrectReviles(int incorrectReviles) {
+        this.incorrectReviles = incorrectReviles;
+    }
+
 
 }
