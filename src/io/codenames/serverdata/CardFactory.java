eda41602d3f860683691be9d78a617c6ac7b3c3f
BuddhiAbeyratne <buddhi741@gmail.com>
2018-11-10 04:14:58 +0530

    implemented flyweight pattern through CardFactory class

--- src/io/codenames/serverdata/CardFactory.java
+++ src/io/codenames/serverdata/CardFactory.java
@@ -1,5 +1,77 @@
 package io.codenames.serverdata;
+import java.util.HashMap;
+import java.util.Random;
 
 public class CardFactory {
+    private HashMap<String, Card> cardMap = new HashMap<String, Card>();
 
+    int blueCount = 0;
+    int blackCount = 0;
+    int redCount = 0;
+    int neutralCount = 0;
+
+    public CardFactory() {
+        Card card;
+        do {
+            String code =  WordList.getWord();
+            card = generateCard(code);
+        }while(card != null);
+
+    }
+
+
+    public Card generateCard(String code) {
+
+        Card card =  cardMap.get(code);
+        if(card ==null){
+            int type = this.randType();
+            if(type == 0){
+                return null;
+            }else {
+                card = new Card(type, code);
+                cardMap.put(code, card);
+            }
+        }
+        return card;
+
+    }
+
+    public HashMap<String, Card> getCardMap() {
+        return cardMap;
+    }
+
+    private int randType(){
+        Random rn = new Random();
+        int type = rn.nextInt(4)+1;
+
+        if((blueCount+blackCount+redCount+neutralCount) >= 25){
+            return 0;
+        }else if(type==1){
+            if(blueCount >= 8 ){
+                return randType();
+            }else {
+                blueCount++;
+            }
+        }else if(type == 2){
+            if(blackCount >= 1 ){
+                return randType();
+            }else {
+                blackCount++;
+            }
+        }else if(type == 3){
+            if(redCount >= 8 ){
+                return randType();
+            }else {
+                redCount++;
+            }
+        }else if(type == 4){
+            if(neutralCount >= 8 ){
+                return randType();
+            }else {
+                neutralCount++;
+            }
+        }
+
+        return type;
+    }
 }
