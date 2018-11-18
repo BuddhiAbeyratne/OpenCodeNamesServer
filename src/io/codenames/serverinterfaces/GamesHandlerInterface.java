0ac186678e011f2313af489486ba9541207a931c
BuddhiAbeyratne <buddhi741@gmail.com>
2018-11-18 16:34:52 +0530

    Create GameHandlerInterface Interface methods

--- src/io/codenames/serverinterfaces/GamesHandlerInterface.java
+++ src/io/codenames/serverinterfaces/GamesHandlerInterface.java
@@ -1,8 +1,16 @@
 package io.codenames.serverinterfaces;
 
+import io.codenames.serverdata.Game;
+
 import java.rmi.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public interface GamesHandlerInterface extends Remote{
 
-	
+	String createGame(String gameName,String creatorName, int numPlayers) throws RemoteException;
+    boolean joinGameQueue(String gameID, String playerName) throws RemoteException;
+    boolean leaveGameQueue(String gameID, String playerName) throws RemoteException;
+    LinkedHashMap<String, HashMap<String,String>> getGames() throws RemoteException ;
+    boolean cardSelected(String gameID, int cardID, String playerName) throws RemoteException;
 }
